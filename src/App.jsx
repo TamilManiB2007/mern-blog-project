a2ed54afa169665b297e1b0d3c769e0d7385565a
TamilManiB2007 <24081.cs@rmkcet.ac.in>
2025-12-08 17:21:22 +0530

    FINAL FIX: Removed all Cursor references from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Write from "./pages/Write";
 import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Portfolio from "./pages/Portfolio";
+
+// ❌ NOTE: Cursor import is REMOVED to fix the Uncaught ReferenceError ❌
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="app">
-
-      {/* 2. FLOATING BACKGROUND BLOBS */}
-      <div className="background-blobs">
-        <div className="blob blob-1"></div>
-        <div className="blob blob-2"></div>
-        <div className="blob blob-3"></div>
-      </div>
-
       <div className="container">
         <BrowserRouter>
-          <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Layout><Home /></Layout>} />
+            <Route path="/post/:id" element={<Layout><Single /></Layout>} />
+            <Route path="/write" element={<Layout><Write /></Layout>} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/write" element={<Write />} />
-            <Route path="/post/:id" element={<Single />} />
-            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/portfolio" element={<Layout><Portfolio /></Layout>} />
           </Routes>
-          <Footer />
         </BrowserRouter>
       </div>
     </div>
